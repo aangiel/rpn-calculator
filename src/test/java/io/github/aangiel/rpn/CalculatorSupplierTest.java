76df5d06a2a3ac72ee3ce26b44bf98a8547d0e84
Artur Angiel <aangiel@tuta.io>
2020-04-04 13:48:07 +0200

    tests corrected

--- src/test/java/io/github/aangiel/rpn/CalculatorSupplierTest.java
+++ src/test/java/io/github/aangiel/rpn/CalculatorSupplierTest.java
@@ -1,11 +1,21 @@
 package io.github.aangiel.rpn;
 
+import io.github.aangiel.rpn.translation.Languages;
+import io.github.aangiel.rpn.translation.Messages;
+import io.github.aangiel.translator.MessageTranslator;
 import org.apfloat.Apfloat;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorSupplierTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        MessageTranslator.setAnyMessage(Messages.EMPTY_EQUATION);
+        MessageTranslator.setLanguage(Languages.EN);
+    }
 
     @Test(expected = ArithmeticException.class)
     public void testCalculator() {
